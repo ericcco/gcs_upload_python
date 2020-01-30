bd1aa8aacb8f0a742d23a76e41d2f72de3b8785a
ericcco <eric.espinola@e-campus.uab.cat>
2020-01-30 13:26:21 +0100

    Create upload_dir_to_gcs.py

---
+++ upload_dir_to_gcs.py
@@ -0,0 +1,25 @@
+import os
+from google.cloud import storage
+
+#change "rootdir" and "bucket_name" for your specific values
+rootdir = '[YOUR_DIRECTORY]/'
+bucket_name = '[BUCKET_NAME]'
+ 
+def upload_blob(source_file_name, destination_blob_name):
+ 
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+ 
+    blob.upload_from_filename(source_file_name)
+ 
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+ 
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        upload_blob(os.path.join(subdir, file), os.path.join(subdir, file))
+        print(os.path.join(subdir, file))
